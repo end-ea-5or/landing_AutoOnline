c2663482696ccdf5b438256b18f29af33da71a27
Andrei Asipchuk <marsel20002008@yandex.ru>
2022-09-01 19:41:17 +0300

    Верстка блока header и promo

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -1,10 +1,6 @@
 // Данный файл - лишь собрание подключений готовых компонентов.
 // Рекомендуется создавать отдельный файл в папке components и подключать все там
 
-// Определение операционной системы на мобильных
-import { mobileCheck } from "./functions/mobile-check";
-console.log(mobileCheck())
-
 // Определение ширины экрана
 // import { isMobile, isTablet, isDesktop } from './functions/check-viewport';
 // if (isDesktop()) {

4ebc1362dc415a5388c98a258683ba3cb3f14d04
Andrei Asipchuk <marsel20002008@yandex.ru>
2022-09-10 22:29:21 +0300

    Update 2022-09-10T19:28:56.879Z

--- src/js/components/form.js
+++
@@ -1,7 +0,0 @@
-import Inputmask from "inputmask";
-
-// Inputmask
-const form = document.querySelector('.modal__form');
-const telSelector = form.querySelector('input[type="tel"]');
-const inputMask = new Inputmask('+7 (999) 999-99-99');
-inputMask.mask(telSelector);
--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-// import './_vendor';
-// import './_functions';
-// import './_components';
-import './components/accordion';
-import './components/countdown';
-import './components/modal';
-import './components/form';

e5812caebf1e65e2d80c35c8493e04c3c4367620
Andrei Asipchuk <marsel20002008@yandex.ru>
2022-09-21 21:53:31 +0300

    Удалил типограф со сборки gulp. Делаю адаптив.

--- gulpfile.js
+++ gulpfile.js
@@ -274,9 +274,9 @@ const htmlInclude = () => {
       prefix: '@',
       basepath: '@file'
     }))
-    .pipe(typograf({
-      locale: ['ru', 'en-US']
-    }))
+    // .pipe(typograf({
+    //   locale: ['ru', 'en-US']
+    // }))
     .pipe(dest(buildFolder))
     .pipe(browserSync.stream());
 }
